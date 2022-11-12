475d9f49b5cfa08b03f74806417da745ff81230d
Joao Paulo <joaopgioio@gmail.com>
2022-11-12 10:49:28 -0300

    alteracao do endpoint

--- src/services/RegistroDePontoService.ts
+++ src/services/RegistroDePontoService.ts
@@ -4,6 +4,7 @@ import IRegistroDePontoAgg, {
 } from "../models/IRegistroDePontoAgg";
 import IRegistroDePontoCreateDto from "../models/IRegistroDePontoCreateDto";
 
+//const BASE_URL = "http://localhost:8085/api/v1/registros";
 const BASE_URL = "https://ponto-all.herokuapp.com/api/v1/registros";
 
 export function getRegistrosDePontoByProfessorId(idProfessor: number) {
