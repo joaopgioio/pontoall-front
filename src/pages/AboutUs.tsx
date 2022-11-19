2f95ba4be588f40e1752c1ec218edea5146e877b
Joao Paulo <joaopgioio@gmail.com>
2022-11-19 10:03:42 -0300

    ajustando responsividade

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -26,12 +26,12 @@ export default function AboutUs() {
           <p>Luciana de Souza Lucena, 2015533</p>
           <p>Marisa da Silva Lemos, 2003931</p>
           <p>João Paulo Patuci Alves, 1703700</p>
-          <p> </p>
-          <p> </p>
-          <p> </p>
-          <p> </p>
-          <p> </p>
-          <p> </p>
+          <p>João Paulo Patuci Alves, 1703700</p>
+          <p>João Paulo Patuci Alves, 1703700</p>
+          <p>João Paulo Patuci Alves, 1703700</p>
+          <p>João Paulo Patuci Alves, 1703700</p>
+          <p>João Paulo Patuci Alves, 1703700</p>
+          <p>João Paulo Patuci Alves, 1703700</p>
         </section>
       </main>
     </Container>
