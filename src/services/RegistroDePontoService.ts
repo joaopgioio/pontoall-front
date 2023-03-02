5135cc6011fb2df1392da2c6b9432a19b64f182a
João Paulo Patuci Alves <joaopgioio@gmail.com>
2023-03-02 03:12:02 -0300

    Update RegistroDePontoService.ts
    Alterado url do heroku para AZURE

--- src/services/RegistroDePontoService.ts
+++ src/services/RegistroDePontoService.ts
@@ -5,7 +5,7 @@ import IRegistroDePontoAgg, {
 import IRegistroDePontoCreateDto from "../models/IRegistroDePontoCreateDto";
 
 //const BASE_URL = "http://localhost:3000/api/v1/registros";
-const BASE_URL = "https://ponto-all.herokuapp.com/api/v1/registros";
+const BASE_URL = "https://registropontoall.azurewebsites.net/api/v1/registros";
 
 export function getRegistrosDePontoByProfessorId(idProfessor: number) {
   return axios.get<IRegistroDePontoAgg[]>(
